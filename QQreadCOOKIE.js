5ebbc135d4579b3f1c7ef3a2e190cdd43c74f03a
tommyhe1217 <66045818+tommyhe1217@users.noreply.github.com>
2021-02-25 21:22:06 +0800

    Create QQreadCOOKIE.js

---
+++ QQreadCOOKIE.js
@@ -0,0 +1,17 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let QQreadurlVal = `https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywgGmhWn00Zp&ywguid=64871872
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywSyd7wLuPis&ywguid=3367037545
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywVcFUPvHd4C&ywguid=348413079
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywlKRwyft3so&ywguid=344741016
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=yw3pTGqBmsLV&ywguid=3224512178`
+
+
+
+
+let QQreadurlcookie = {
+
+    QQreadurlVal: QQreadurlVal,
+
+}
+
+module.exports = QQreadurlcookie
